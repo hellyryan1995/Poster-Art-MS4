a2441f98a459b623b2be000be5e3e226bda4f8c8
hellyryan1995 <helly.ryan1995@gmail.com>
2021-10-12 23:01:08 +0000

    Removed secret key and set debug

--- poster_art/settings.py
+++ poster_art/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8cc12r13ghm-e2=858hf$+6o-abw4y9*qjm694$+0^@5yk1mb'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['poster-art-ms4.herokuapp.com', 'localhost']
 

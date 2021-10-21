13ea126653f9754feb09f486963f0b926d1ac340
hellyryan1995 <helly.ryan1995@gmail.com>
2021-10-21 12:08:27 +0000

    Added product size logic

--- products/models.py
+++ products/models.py
@@ -19,19 +19,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    # SIZE_POSTER = (
-    #     ('A4: 21.0 x 29.7cm', 'A4: 21.0 x 29.7cm'),
-    #     ('A3: 29.7 x 42.0cm', 'A3: 29.7 x 42.0cm'),
-    #     ('A2: 42.0 x 59.4cm', 'A2: 42.0 x 59.4cm'),
-    # )
 
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    # size = MultiSelectField(
-    #     choices=SIZE_POSTER, null=True, blank=True)
+    size = models.BooleanField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 

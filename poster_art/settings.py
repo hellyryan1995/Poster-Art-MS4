ae5f40749ab98584e1afc0f7ea4d0af70f611b10
hellyryan1995 <helly.ryan1995@gmail.com>
2021-10-23 10:06:28 +0000

    Fixed MEDIA_URL to display images

--- poster_art/settings.py
+++ poster_art/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
         },
@@ -163,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
+# STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
